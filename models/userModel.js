fddda0dfcd6cdcd26fc07f4f4c824614442e9a38
ShailySarker <shailysarker99@gmail.com>
2025-02-20 17:20:25 +0600

    all models are added

---
+++ models/userModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    username: { type: String, unique: true, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    image: { type: String, required: true },
+    accountType: {
+        type: String,
+        enum: ['user', 'admin'],
+        default: 'user',
+    },
+    token: { type: String },
+    profile: {
+        firstName: { type: String },
+        lastName: { type: String },
+        dateOfBirth: { type: Date },
+    },
+    createdAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
