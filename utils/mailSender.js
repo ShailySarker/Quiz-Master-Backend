e092313a4121e4f41b83b1f20451c10195fedf7a
ShailySarker <shailysarker99@gmail.com>
2025-02-20 18:29:22 +0600

    some functionality updated

--- utils/mailSender.js
+++ utils/mailSender.js
@@ -1,26 +1,27 @@
 const nodemailer = require("nodemailer")
 
-const mailSender = async (email,title,body)=>{
-      try{
+const mailSender = async (email, title, body) => {
+    try {
         let transporter = nodemailer.createTransport({
-            host:process.env.MAIL_HOST,
-            auth:{
-                user:process.env.MAIL_USER,
-                pass:process.env.MAIL_PASS,
+            // host: process.env.MAIL_HOST,
+            service: 'gmail',
+            auth: {
+                user: process.env.MAIL_USER,
+                pass: process.env.MAIL_PASS,
             }
         })
         // Send mail
-          let info  = await transporter.sendMail({
-           from:`shaillysarker11@gmail.com`,
-           to:`${email}`,
-           subject:`${title}`,
-           html:`${body}`
-          })
-          console.log(info);
-          return info;
-      }  catch(error){
+        let info = await transporter.sendMail({
+            from: process.env.MAIL_USER,
+            to: `${email}`,
+            subject: `${title}`,
+            html: `${body}`
+        })
+        console.log(info);
+        return info;
+    } catch (error) {
         console.log(error);
-      }
+    }
 }
 
 module.exports = mailSender;
