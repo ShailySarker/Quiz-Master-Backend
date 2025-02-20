8db98bfacd9123ec9abb47910ead372dc72b9de4
ShailySarker <shailysarker99@gmail.com>
2025-02-21 01:15:43 +0600

    live link added

--- utils/mail/emailVarificationFormat.js
+++ utils/mail/emailVarificationFormat.js
@@ -71,18 +71,18 @@ const otpTemplate = (otp) => {
 	
 	<body>
 		<div class="container">
-			<h1>My Quiz App</h1>
+			<h1>Quiz Master</h1>
 			<div class="message">OTP Verification Email</div>
 			<div class="body">
 				<p>Dear User,</p>
-				<p>Thank you for registering with Our Quiz app. To complete your registration, please use the following OTP
+				<p>Thank you for registering with our Quiz Master website. To complete your registration, please use the following OTP
 					(One-Time Password) to verify your account:</p>
 				<h2 class="highlight">${otp}</h2>
 				<p>This OTP is valid for 5 minutes. If you did not request this verification, please disregard this email.
 				Once your account is verified, you will have access to our platform and its features.</p>
 			</div>
 			<div class="support">If you have any questions or need assistance, please feel free to reach out to us at <a
-					href="mailto:info@quizgame.com">info@quizGame.com</a>. We are here to help!</div>
+					href="mailto:info@quizmaster.com">info@quizmaster.com</a>. We are here to help!</div>
 		</div>
 	</body>
 	
